736af786f818cc3439e7e2fc317ac017a4aefcba
Cristian G Guerrero <guyik.cgg@gmail.com>
2019-01-17 00:24:50 +0100

    Enhance border calculations

--- centers2borders.m
+++ centers2borders.m
@@ -7,8 +7,14 @@ function[borders] = centers2borders(centers)
 
 % TODO borders can be better calculated
 
-sizes = diff(centers);
-sizes = [mean(sizes) sizes mean(sizes)];
-shift = sizes./2;
-borders = [(centers(1) - shift(1)), (centers .+ shift(2:end))];
+size_0 = median(diff(centers))/2;
+borders = zeros(1, length(centers)+1);
+borders(1) = centers(1)-size_0;
+for i=2:length(centers)
+	borders(i) = mean(centers(i-1:i));
+end
+borders(end) = centers(end)+size_0;
+% sizes = [mean(sizes) sizes mean(sizes)];
+% shift = sizes./2;
+% borders = [(centers(1) - shift(1)), (centers .+ shift(2:end))];
 borders = round(borders);
